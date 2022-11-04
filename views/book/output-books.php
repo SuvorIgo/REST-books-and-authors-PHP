ad5df308609bac6daf04749ed21798b8d58edcbe
Igor <suekandig@mail.ru>
2022-11-04 16:20:41 +0300

    correction view output-books

--- views/book/output-books.php
+++ views/book/output-books.php
@@ -1,4 +1,12 @@
 <?php
 use yii\helpers\Html;
 ?>
-<?= Html::encode($id) ?>
+<h1>Books</h1>
+<ul>
+    <?php foreach ($books as $book) : ?>
+        <li>
+            <? Html::encode("{$book->id} ({$country->name})") ?> :
+            <? $book->numberOfPages ?>
+        </li>
+    <?php endforeach; ?>
+</ul>
