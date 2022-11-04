977538ae98e2c5a63e4149570916e2d9340d7c1e
Igor <suekandig@mail.ru>
2022-11-04 13:48:16 +0300

    creating a database connection

---
+++ models/Author.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "Author".
+ *
+ * @property int $id
+ * @property string $surname
+ * @property string $name
+ * @property string $patronymic
+ * @property string|null $nickname
+ *
+ * @property Book[] $books
+ */
+class Author extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'Author';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['surname', 'name', 'patronymic'], 'required'],
+            [['surname', 'name', 'patronymic', 'nickname'], 'string', 'max' => 100],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'surname' => 'Surname',
+            'name' => 'Name',
+            'patronymic' => 'Patronymic',
+            'nickname' => 'Nickname',
+        ];
+    }
+
+    /**
+     * Gets query for [[Books]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getBooks()
+    {
+        return $this->hasMany(Book::class, ['id_author' => 'id']);
+    }
+}
