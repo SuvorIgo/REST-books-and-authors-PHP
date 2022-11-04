2e15e0d25a289e7b54b64e41689272a5974b774b
Igor <suekandig@mail.ru>
2022-11-04 14:00:05 +0300

    creating CRUD operations for Author

---
+++ views/author/_search.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/** @var yii\web\View $this */
+/** @var app\models\AthorSearch $model */
+/** @var yii\widgets\ActiveForm $form */
+?>
+
+<div class="author-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+    ]); ?>
+
+    <?= $form->field($model, 'id') ?>
+
+    <?= $form->field($model, 'surname') ?>
+
+    <?= $form->field($model, 'name') ?>
+
+    <?= $form->field($model, 'patronymic') ?>
+
+    <?= $form->field($model, 'nickname') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton('Reset', ['class' => 'btn btn-outline-secondary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
