715c95836c1b7c09af57f27ee8a8baa980aa60a7
Igor <suekandig@mail.ru>
2022-11-05 18:26:38 +0300

    attempt to solve the error in action

--- controllers/BookController.php
+++ controllers/BookController.php
@@ -117,17 +117,29 @@ class BookController extends Controller
         return $this->redirect(['index']);
     }
 
-
     public function actionOutputBooks($id = NULL){
         $author = Author::findOne($id);
         $books = Book::find();
 
-        $books = $author->books;
+        if ($id === NULL)
+        {
+            $authors = Author::find()->all();
+            $books = $authors->books;
 
-        return $this->render('output-books', [
-            'author' => $author,
-            'books' => $books
-        ]);
+            return $this->render('output-books', [
+                'authors' => $authors,
+                'books' => $books
+            ]);
+        }
+        else
+        {
+            $books = $author->books;
+
+            return $this->render('output-books', [
+                'author' => $author,
+                'books' => $books
+            ]);
+        }
     }
     /**
      * Finds the Book model based on its primary key value.
